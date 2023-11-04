7c86fe73b1abdaafe96b99cae4f3055b41508f01
MostafaMemari <144701776+MostafaMemari@users.noreply.github.com>
2023-11-04 18:28:45 +0330

    create all Model

---
+++ model/Academic/yearGroupModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const yearGroupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Admin",
+      required: true,
+    },
+    academicYear: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "AcademicYear",
+      required: true,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+//model
+const yearGroupModel = mongoose.model("YearGroup", yearGroupSchema);
+
+module.exports = { yearGroupModel };
